a695f1b127edf556d6a09ea9555ad5c0d35d35cf
Vaishnav Anil <vaishnavanil7th@gmail.com>
2022-03-28 16:45:16 +0530

    Added ObjectJsonNodeTest

--- src/test/java/io/github/vshnv/blueprint/ObjectJsonNodeTest.java
+++ src/test/java/io/github/vshnv/blueprint/ObjectJsonNodeTest.java
@@ -10,8 +10,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ObjectJsonNodeTest {
     private static final Gson GSON = new Gson();
+
     @Test
-    public void testJsonEncode() {
+    public void testSimpleObject() {
+        final JsonNode node = obj(
+                name -> lit("Josh"),
+                age -> lit(25),
+                nickname -> lit("Tesla")
+        );
+        final JsonObject expectedObject = new JsonObject();
+        expectedObject.addProperty("name", "Josh");
+        expectedObject.addProperty("age", 25);
+        expectedObject.addProperty("nickname", "Tesla");
+        final JsonObject actualObject = GSON.fromJson(node.toString(), JsonObject.class);
+        assertEquals(expectedObject, actualObject);
+    }
+
+    @Test
+    public void testComplexObject() {
         final JsonNode node = obj(
                 name -> lit("Josh"),
                 age -> lit(25),
@@ -20,13 +36,8 @@ class ObjectJsonNodeTest {
                         city -> lit("New York"),
                         state -> lit("NY")
                 ),
-                friends -> arr(
-                        obj(
-                                name -> lit("John")
-                        ),
-                        obj(
-                                name -> lit("Jane")
-                        )
+                friend -> obj(
+                        name -> lit("John")
                 )
         );
         final JsonObject expectedObject = new JsonObject();
@@ -37,14 +48,9 @@ class ObjectJsonNodeTest {
         address.addProperty("city", "New York");
         address.addProperty("state", "NY");
         expectedObject.add("address", address);
-        final JsonObject john = new JsonObject();
-        john.addProperty("name", "John");
-        final JsonObject jane = new JsonObject();
-        jane.addProperty("name", "Jane");
-        final JsonArray friends = new JsonArray();
-        friends.add(john);
-        friends.add(jane);
-        expectedObject.add("friends", friends);
+        final JsonObject friend = new JsonObject();
+        friend.addProperty("name", "John");
+        expectedObject.add("friend", friend);
         final JsonObject actualObject = GSON.fromJson(node.toString(), JsonObject.class);
         assertEquals(expectedObject, actualObject);
     }
