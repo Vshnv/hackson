b6a94a6307e2b47534913fc9e6a5cf409c447175
Vaishnav Anil <vaishnavanil7th@gmail.com>
2022-04-03 02:24:44 +0530

    Added NumberLiteralJsonNode & NumberLiteralJsonNodeReader

---
+++ src/main/java/io/github/vshnv/blueprint/literal/NumberLiteralJsonNode.java
@@ -0,0 +1,53 @@
+package io.github.vshnv.blueprint.literal;
+
+import io.github.vshnv.blueprint.IndexedString;
+import io.github.vshnv.blueprint.LiteralJsonNode;
+import io.github.vshnv.blueprint.reader.JsonNodeReader;
+
+import static io.github.vshnv.blueprint.ParsingUtils.parseAssert;
+
+public final class NumberLiteralJsonNode implements LiteralJsonNode<Number> {
+    private final JsonNodeReader<Number> reader;
+    private Number number;
+
+    public NumberLiteralJsonNode(final JsonNodeReader<Number> reader, final Number number) {
+        this.reader = reader;
+        this.number = number;
+    }
+
+    @Override
+    public void writeTo(final StringBuffer buffer) {
+        buffer.append(number);
+    }
+
+    @Override
+    public void match(final IndexedString indexedString) {
+        final Number number = reader.read(indexedString);
+        parseAssert(approxEquals(this.number, number));
+    }
+
+    @Override
+    public Number getValue() {
+        return number;
+    }
+
+    @Override
+    public void setValue(final Number number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return stringify();
+    }
+
+    private static boolean approxEquals(final Number a, final Number b) {
+        if (a == null) {
+            return b == null;
+        } else if (b == null) {
+            return false;
+        }
+
+        return Math.abs(a.floatValue() - b.floatValue()) < 0.0000001;
+    }
+}
