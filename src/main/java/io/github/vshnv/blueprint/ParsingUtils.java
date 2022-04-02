9e7cbb07c0c679964a67675a19840fd87b4969fa
Vaishnav Anil <vaishnavanil7th@gmail.com>
2022-04-03 02:25:52 +0530

    updated ParsingUtils

--- src/main/java/io/github/vshnv/blueprint/ParsingUtils.java
+++ src/main/java/io/github/vshnv/blueprint/ParsingUtils.java
@@ -1,5 +1,10 @@
 package io.github.vshnv.blueprint;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 public class ParsingUtils {
     private ParsingUtils() {
         throw new AssertionError("No instances");
@@ -15,5 +20,17 @@ public class ParsingUtils {
         while (s.getCharAtIndex() == ' ') {
             s.incrementIndex();
         }
+    }
+
+    public static String matchAny(final IndexedString s, final String... words) {
+        skipSpaces(s);
+        final List<String> matches = new ArrayList<>(Arrays.asList(words));
+        int index = 0;
+        while (matches.size() > 1 && index < s.getString().length()) {
+            matches.removeIf(word -> word.charAt(index) != s.getCharAtIndex());
+            s.incrementIndex();
+        }
+        parseAssert(matches.size() == 1);
+        return matches.get(0);
     }
 }
